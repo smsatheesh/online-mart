20ab7892466dd5a5b4648b4d3982d9d5d8e3060f
smsatheesh <smsatheesh2000@gmail.com>
2026-06-23 13:29:50 +0530

    Add initial project structure with Maven configuration, application properties, and main application class

---
+++ src/main/java/com/onlinemart/online_mart_platform/OnlineMartPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.onlinemart.online_mart_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMartPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMartPlatformApplication.class, args);
+	}
+
+}
